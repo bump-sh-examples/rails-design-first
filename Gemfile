0b2b43e2a088c0b849361bfc9382a46caee1c728
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2024-01-09 12:42:19 +0000

    switch to upstream main openapi_contracts

--- Gemfile
+++ Gemfile
@@ -6,13 +6,11 @@ gem "rails", "~> 7.1.2"
 gem "sqlite3", "~> 1.4"
 gem "puma", ">= 5.0"
 
-gem 'pry', '~> 0.14.2'
-
 gem 'openapi_first', '~> 1.0'
-gem 'openapi_contracts', git: "git@github.com:philsturgeon/openapi_contracts.git"
+gem 'openapi_contracts', git: "git@github.com:mkon/openapi_contracts.git"
 
 group :test do
-  gem 'rspec-rails', '~> 6.0.0'
+  gem 'rspec-rails', '~> 6.1.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = ["#{::Rails.root}/spec/fixtures"]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -41,19 +41,6 @@ RSpec.configure do |config|
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://rspec.info/features/6-0/rspec-rails
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
